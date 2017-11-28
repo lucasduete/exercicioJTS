9084d28eb7bb61766f687cd78872f5289bbad31e
Lucas Duete <lm.duete@gmail.com>
2017-11-28 10:59:47 -0300

    Adicionando Classe para Calcular ViewBox e SVG.

---
+++ src/main/java/io/github/lucasduete/controller/SVGFactory.java
@@ -0,0 +1,37 @@
+
+package io.github.lucasduete.controller;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+
+public class SVGFactory {
+   
+    public static String getSVG (Geometry geometria) {
+        
+        StringBuilder builder = new StringBuilder();
+        
+        Coordinate[] coordenadas = geometria.getCoordinates();
+        
+        builder.append("M");
+        builder.append(" ");
+        builder.append(coordenadas[0].x);
+        builder.append(" ");
+        builder.append(coordenadas[0].y);
+        
+        builder.append(" ");
+        
+        for(Coordinate c: coordenadas) {
+            builder.append("L");
+            builder.append(" ");
+            builder.append((Double) c.x);
+            builder.append(" ");
+            builder.append((Double) c.y);
+            builder.append(" ");
+        }
+        
+        builder.append("Z");
+        
+        return builder.toString();        
+    }
+}
+
